4040cfad574b0e2c24b7db9efa6ff07f662efad1
河相　尚登 <h_kawai@plsize.co.jp>
2016-05-09 16:40:26 +0900

    0509 first commit

--- src/CalculateSales.java
+++ src/CalculateSales.java
@@ -34,10 +34,15 @@ public class CalculateSales
 //				if(!m.find())
 				{
 					System.out.println("支店名義ファイルのフォーマットが不正です");
+					br.close();
+					return;
 				}
 
 				branch.put(braSpl[0], braSpl[1]);
 				braSum.put(braSpl[0], 0L);
+				{
+
+				}
 			}
 			//{
 				//System.out.println(str);
@@ -48,6 +53,7 @@ public class CalculateSales
 		catch(FileNotFoundException e)
 		{
 			System.out.println("支店定義ファイルが存在しません");
+			return;
 		}
 		catch(IOException e)
 		{
@@ -76,6 +82,8 @@ public class CalculateSales
 				if(!comSpl[0].matches("^\\w{8}$"))
 				{
 					System.out.println("商品定義ファイルのフォーマットが不正です");
+					br.close();
+					return;
 				}
 
 
@@ -88,6 +96,7 @@ public class CalculateSales
 		catch(FileNotFoundException e)
 		{
 			System.out.println("商品定義ファイルが存在しません");
+			return;
 		}
 		catch(IOException e)
 		{
@@ -96,7 +105,7 @@ public class CalculateSales
 		System.out.println(commodity.entrySet());
 		System.out.println(comSum.entrySet());
 
-//集計
+//rcdファイルだけ
 		File dir = new File(args[0]);
 		File files[] = dir.listFiles();
 
@@ -107,19 +116,33 @@ public class CalculateSales
 		{
 			if(files[i].getName().endsWith(".rcd"))
 			{
+
+//8桁
 				String[] fileSpl = files[i].getName().toString().split("\\.");
+				int j = 0;
+				if(fileSpl[0].matches("^\\w{8}$"))
+				{
+					 j = Integer.parseInt(fileSpl[0]);
+				}
+				else
+				{
+					System.out.println(files[i] + "は売上ファイル名が8桁ではありません");
+					return;
+				}
 				//rcdNo.add( new Integer(fileSpl[0]).intValue());
-				int j = Integer.parseInt(fileSpl[0]);
 
 
-				//System.out.println(rcdNo);
+
+
+//連番処理
 				if(j - 1 == i)
 				{
 					rcdList.add(files[i]);
 				}
 				else
 				{
-					System.out.println("売上ファイル名が連番になっていません");
+					System.out.println(files[i] + "は売上ファイル名が連番になっていません");
+					return;
 				}
 			}
 		}
@@ -138,26 +161,58 @@ public class CalculateSales
 				{
 					inrcdFile.add(sale);
 				}
-				System.out.println(inrcdFile.get(0));
-				System.out.println(inrcdFile.get(1));
-				System.out.println(inrcdFile.get(2));
+				
+				if(inrcdFile.size() != 3)
+				{
+					System.out.println(rcdList.get(k).getName() + "のフォーマットが不正です");
+					br.close();
+					return;
+				}
+
+				//System.out.println(listStr);
+				//System.out.println(rcdList.get(k));
+				//System.out.println(rcdList.get(k).getName());
+
+				System.out.println(inrcdFile.get(0));//=支店コード
+				System.out.println(inrcdFile.get(1));//=商品コード
+				System.out.println(inrcdFile.get(2));//=売上額
+
+				//if()
 
 				long l = Long.parseLong(inrcdFile.get(2));
 
 				long bratotal = l + braSum.get(inrcdFile.get(0));
 				long comtotal = l + comSum.get(inrcdFile.get(1));
-			
+
 				braSum.put(inrcdFile.get(0), bratotal);
 				comSum.put(inrcdFile.get(1), comtotal);
+
+//10桁を超えた場合のエラー処理
+				int sbt = String.valueOf(bratotal).length();
+				if(sbt > 10)
+				{
+					System.out.println(inrcdFile.get(0) + "は合計金額が10桁を超えました");
+					br.close();
+					return;
+				}
+
+				int sct = String.valueOf(comtotal).length();
+				if(sct > 10)
+				{
+					System.out.println(inrcdFile.get(1) + "は合計金額が10桁を超えました");
+					br.close();
+					return;
+				}
+
 
 				//System.out.println(l);
 
 
 
-			br.close();
-			System.out.println(braSum.entrySet());
-			System.out.println(comSum.entrySet());
-			System.out.println("^^^^^^");
+				br.close();
+				System.out.println(braSum.entrySet());
+				System.out.println(comSum.entrySet());
+				System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
 			}
 
 		}
